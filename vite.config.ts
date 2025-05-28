c7664270384e115d77fb150cbaec561b01c60c6c
Sahil-Sinha-11 <sahilsinha6969@gmail.com>
2025-05-28 14:33:58 +0530

    deploy changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/grit-india",
+  // try :-pubfest-2025
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
